23b36864fb0562a505e6566828d3ad157b7b10e4
Juan C Felizzola <juancafelizzola@gmail.com>
2021-09-29 20:03:58 -0500

    form and sizes

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,10 +5,30 @@ import IconAttendance from "../assets/attendance.svg";
 import IconCellsGroups from "../assets/cells-groups.svg";
 
 const Hero = ({ fluid }) => {
+  const [number, setNumber] = React.useState(undefined)
+
+  const handleChange = ({ target }) => {
+    setNumber(target.number)
+  }
+
+  const handleClick = async () => {
+    const response = await fetch("http://dashboard.iglenube.com/llamame", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        telefono: number
+      })
+    })
+
+    console.log(response.json())
+  }
+  
   return (
     <div className="container grid mx-auto h-5/6 sm:grid-cols-2 sm:gap-10">
       <div className="px-3 mt-4 sm:p-20">
-        <h1 className="mb-4 text-4xl font-semibold text-gray-800 sm:mb-8 sm:text-3xl lg:text-5xl md:text-4xl">
+        <h1 className="mb-4 text-3xl font-semibold text-gray-800 sm:mb-8 lg:text-5xl md:text-4xl">
           Toda la información de tu iglesia a un clic
         </h1>
         <div className="relative">
@@ -16,8 +36,13 @@ const Hero = ({ fluid }) => {
             type="number"
             className="w-full px-4 py-3 rounded focus:outline-none"
             placeholder="Ingresa tu número"
+            onChange={handleChange}
           />
-          <button className="absolute px-2 py-1 font-semibold text-white rounded right-2 top-2 bg-blue-light">
+          <button
+            className="absolute px-2 py-1 font-semibold text-white rounded right-2 top-2 bg-blue-light"
+            type="button"
+            onClick={handleClick}
+          >
             Llámame
           </button>
         </div>
@@ -25,11 +50,11 @@ const Hero = ({ fluid }) => {
       <div className="relative">
         <figure
           style={{ maxWidth: 320 }}
-          className="w-24 max-w-xs mx-auto mt-4 sm:w-52"
+          className="w-32 mx-auto mt-4 ma3x-w-xs sm:w-52"
         >
           <Img fluid={fluid} alt="Persona usando Iglenube en un iPad" />
         </figure>
-        <div className="absolute flex gap-2 px-4 py-2 bg-white rounded-lg shadow-xl sm:bottom-40 sm:left-32 bottom-6 left-6">
+        <div className="absolute flex gap-2 px-4 py-2 bg-white rounded-lg shadow-xl sm:bottom-40 sm:left-32 bottom-16 left-6">
           <div className="p-2 rounded-full bg-blue-light">
             <IconAttendance width={24} height={24} />
           </div>
@@ -38,7 +63,7 @@ const Hero = ({ fluid }) => {
             <p className="text-sm text-gray-dark">Finanzas</p>
           </div>
         </div>
-        <div className="absolute flex gap-2 px-4 py-2 bg-white rounded-lg shadow-xl sm:bottom-64 sm:right-32 bottom-24 right-6">
+        <div className="absolute flex gap-2 px-4 py-2 bg-white rounded-lg shadow-xl sm:bottom-64 sm:right-32 bottom-40 right-6">
           <div className="p-2 rounded-full bg-blue-light">
             <IconCellsGroups width={24} height={24} />
           </div>
